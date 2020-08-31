f320459394176ac688c35b974734089beccad0b9
Arnav Dixit <arnavdixit127@gmail.com>
2020-08-31 20:40:58 +0530

    Minor Fixes

--- getmeetingwithID.go
+++ getmeetingwithID.go
@@ -7,28 +7,24 @@ import (
 	"net/http"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 //GetMeetingwithID : Gives the meeting with the provided id
 func GetMeetingwithID(response http.ResponseWriter, request *http.Request) {
-	response.Header().Set("content-type", "application/json")
-	fmt.Println((request.URL.Query()["start"][0]))
-	fmt.Println((request.URL.Query()["end"][0]))
-	CheckstartTime := request.URL.Query()["start"][0]
-	CheckendTime := request.URL.Query()["end"][0]
-	collection := client.Database("appointy").Collection("meetings")
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer cancel()
-	cursor, _ := collection.Find(ctx, bson.M{})
-	var meetingsreturn []Meeting
-	var meet Meeting
-	for cursor.Next(ctx) {
-		cursor.Decode(&meet)
-		if (CheckstartTime <= meet.Starttime) && (CheckendTime >= meet.Endtime) {
-			meetingsreturn = append(meetingsreturn, meet)
+	if request.Method == "GET" {
+		response.Header().Set("content-type", "application/json")
+		fmt.Println((request.URL.Query()["id"][0]))
+		id, _ := primitive.ObjectIDFromHex(request.URL.Query()["id"][0])
+		var meet Meeting
+		collection := client.Database("appointy").Collection("meetings")
+		ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+		err := collection.FindOne(ctx, Meeting{ID: id}).Decode(&meet)
+		if err != nil {
+			response.WriteHeader(http.StatusInternalServerError)
+			response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+			return
 		}
+		json.NewEncoder(response).Encode(meet)
 	}
-	json.NewEncoder(response).Encode(meetingsreturn)
-
 }
