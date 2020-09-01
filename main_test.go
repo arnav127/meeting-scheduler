5131c0b8d9ae2dec12547913d2ae3e4647fae3aa
Arnav Dixit <arnavdixit127@gmail.com>
2020-09-01 15:34:19 +0530

    Updated Tests

--- main_test.go
+++ main_test.go
@@ -13,8 +13,45 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func TestMain(t *testing.T) {
+func TestMainCreatemeet(t *testing.T) {
+	var message Meeting
+	var part participant
+	part.Name = "Arnav Dixit"
+	part.Email = "arnavdixit127@gmail.com"
+	part.Rsvp = "Yes"
+	message.Title = "Title"
+	message.Participants = append(message.Participants, part)
+	message.Starttime = "2021-09-01T09:52:12+05:30"
+	message.Endtime = "2021-09-01T10:52:12+05:30"
+	bytesRepresentation, _ := json.Marshal(message)
+	resp, err := http.Post("http://localhost:12345/meetings", "application/json", bytes.NewBuffer(bytesRepresentation))
+	if err != nil {
+		t.Error("Fail")
+	}
+	if resp == nil {
+		t.Error("NO response")
+	}
+}
+func TestMaingetmeet(t *testing.T) {
 	resp, err := http.Get("http://localhost:12345/meeting/?id=5f4dcb738b246dc74d8ecd44")
+	if err != nil {
+		t.Error("Fail")
+	}
+	if resp == nil {
+		t.Error("NO response")
+	}
+}
+func TestMaingetparticipants(t *testing.T) {
+	resp, err := http.Get("http://localhost:12345/articles/?participant=manast95@gmail.com")
+	if err != nil {
+		t.Error("Fail")
+	}
+	if resp == nil {
+		t.Error("NO response")
+	}
+}
+func TestMaingetmeetintime(t *testing.T) {
+	resp, err := http.Get("http://localhost:12345/meetings?start=2019-09-01T13:30:10+05:30&end=2021-09-01T14:30:10+05:30")
 	if err != nil {
 		t.Error("Fail")
 	}
@@ -48,7 +85,7 @@ func BenchmarkMaingetpost(b *testing.B) {
 
 	bytesRepresentation, err := json.Marshal(message)
 	if err != nil {
-		fmt.Println("Erur")
+		fmt.Println("Error")
 	}
 	for n := 0; n < b.N; n++ {
 		resp, err := http.Post("http://localhost:12345/meetings", "application/json", bytes.NewBuffer(bytesRepresentation))
